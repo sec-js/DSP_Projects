ba059a27789c1077a9671d0fbfc5fb44afd77d0f
Gaetano Perrone <gaetanoperrone@MBP-di-Gaetano.fritz.box>
2017-05-14 08:49:52 +0200

    All

---
+++ .docker-images/dsp_alpine_base/adduser.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Author:       giper
+# Email:        g.per45@gmail.com
+# Date:        	 
+# Usage:        adduser.sh [--name=val] [ --password=val]
+# Description:
+# 
+#
+#
+
+
+usage() {
+echo $1
+echo "Usage adduser.sh [--name=val] [ --password=val]"
+exit 1
+}
+
+Arguments=( "$@" ) 
+# Parse Parameters #
+for ARG in "${Arguments[@]}"; do
+  case $ARG in
+    --name=*)
+      NAME=${ARG#*=} 
+      ;;
+    --password=*)
+      PASSWORD=${ARG#*=} 
+      ;;
+    *)
+      usage "Unknown Argument $ARG" ;;
+  esac
+done
+
+if [ $# -ne 2 ]; then
+   usage "args must be 2"
+fi
+
+
+adduser -D $NAME
+echo $NAME:$PASSWORD | chpasswd

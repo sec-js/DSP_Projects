d841e1946d64b01bab2eec3088850fa02a443d81
Gaetano Perrone <gaetanoperrone@MBP-di-Gaetano.fritz.box>
2017-05-16 08:08:56 +0200

    Add all repos

--- .docker-images/update.sh
+++ .docker-images/update.sh
@@ -4,7 +4,10 @@ cd ..
 }
 
 update_image dsp_alpine_base dsp/alpine_base
-update_image dsp_alpine_router dsp/alpine_router
-update_image dsp_shellinabox dsp/shellinabox
-update_image dsp_linode_lamp/ dsp/linode_lamp
-
+#update_image dsp_alpine_router dsp/alpine_router
+#update_image dsp_shellinabox dsp/shellinabox
+#update_image dsp_alpine_ssh_keys_auth/ dsp/alpine_ssh_keys_auth
+#update_image dsp_alpine_ssh_password_auth/ dsp/alpine_ssh_password_auth
+#update_image dsp_linode_lamp/ dsp/linode_lamp
+#update_image dsp_alpine_telnet/ dsp/alpine_telnet
+#update_image dsp_alpine_bot/ dsp/alpine_bot

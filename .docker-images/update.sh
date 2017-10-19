a2524994ac3f7c2778784636f448562e6bc70718
Gaetano Perrone <>
2017-10-19 04:44:18 +0200

    Update DSP_Projects

--- .docker-images/update.sh
+++ .docker-images/update.sh
@@ -22,15 +22,7 @@ update_image dockersecplayground_alpine_telnet dockersecplayground/alpine_telnet
 update_image dockersecplayground_alpine_test dockersecplayground/alpine_test:latest
 update_image dockersecplayground_ssh_keys dockersecplayground/ssh_keys:latest
 update_image dockersecplayground_alpine_ssh_password dockersecplayground/alpine_ssh_password:latest
-update_image dockersecplayground_shellinabox dockersecplayground/shellinabox:latest
 update_image dockersecplayground_linode_lamp dockersecplayground/linode_lamp:latest
 update_image dockersecplayground_ubuntu32 dockersecplayground/ubuntu32
 
 # Remote Images NO LATEST TO LET THE pull_image function to properly work
-pull_image bkimminich/juice-shop
-pull_image psiinon/bodgeit
-pull_image webgoat/webgoat-7.1
-pull_image citizenstig/dvwa
-pull_image citizenstig/owaspbricks
-pull_image citizenstig/nowasp
-pull_image wpscanteam/vulnerablewordpress

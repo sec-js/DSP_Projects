e82ee988f4799e4a9578f6c0f8e72780766fa6ee
Gaetano Perrone <gaetano.perrone@epsilonline.com>
2018-07-25 15:27:55 +0200

    Fix pull.sh

--- .docker-images/pull.sh
+++ .docker-images/pull.sh
@@ -6,19 +6,20 @@ pull_image() {
   fi
 }
 # Built images
-pull_image dockersecplayground_alpine dockersecplayground/alpine:v1.0
-pull_image dockersecplayground_alpine_ftp dockersecplayground/alpine_ftp:v1.0
-pull_image dockersecplayground_alpine_compiler dockersecplayground/alpine_compiler:v1.0
-pull_image dockersecplayground_alpine_networking dockersecplayground/alpine_networking:v1.0
-pull_image dockersecplayground_alpine_bot dockersecplayground/alpine_bot:v1.0
-pull_image dockersecplayground_alpine_router dockersecplayground/alpine_router:v1.0
-pull_image dockersecplayground_alpine_telnet dockersecplayground/alpine_telnet:v1.0
-pull_image dockersecplayground_alpine_test dockersecplayground/alpine_test:v1.0
-pull_image dockersecplayground_ssh_keys dockersecplayground/ssh_keys:v1.0
-pull_image dockersecplayground_alpine_ssh_password dockersecplayground/alpine_ssh_password:v1.0
-pull_image dockersecplayground_linode_lamp dockersecplayground/linode_lamp:v2.0
-pull_image dockersecplayground_ubuntu32 dockersecplayground/ubuntu32
-pull_image dockersecplayground_kali dockersecplayground/kali
+pull_image dockersecplayground/alpine:v1.0
+pull_image dockersecplayground/alpine_ftp:v1.0
+pull_image dockersecplayground/alpine_compiler:v1.0
+pull_image dockersecplayground/alpine_networking:v1.0
+pull_image dockersecplayground/alpine_bot:v1.0
+pull_image dockersecplayground/alpine_router:v1.0
+pull_image dockersecplayground/alpine_telnet:v1.0
+pull_image dockersecplayground/alpine_test:v1.0
+pull_image dockersecplayground/ssh_keys:v1.0
+pull_image dockersecplayground/alpine_ssh_password:v1.0
+pull_image dockersecplayground/linode_lamp:v1.0
+pull_image dockersecplayground/linode_lamp:v2.0
+pull_image dockersecplayground/ubuntu32
+pull_image dockersecplayground/kali
 
 # Update entrypoint image
 # Remote Images NO v1.0 TO LET THE pull_image function to properly work
